498b8e7e12b83d1809d0ea7b8e91ead8f64f5576
YerikMeza <78888302+YerikMeza@users.noreply.github.com>
2021-08-02 17:39:20 -0800

    Add files via upload

---
+++ Bed_room.js
@@ -0,0 +1,51 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+   img = loadImage("Bed_Room.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(650, 500);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error, results) {
+    if (error) 
+    {
+      console.error(error);
+    }
+    else 
+    {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 650, 500);
+
+    if (status != "") 
+    {
+        for (i = 0; i < objects.length; i++) 
+        {
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,12 @@
+
+function button_Room() {
+    window.location = "Bed_room.html"
+}
+
+function button_Fruit() {
+    window.location = "Fruit.html"
+}
+
+function button_desk() {
+    window.location = "Desk.html"
+}
